c0bb0129a26a401f4a77a943911495d9e87e1467
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-08-04 11:34:49 -0300

    Add files via upload
    lendo int e string

---
+++ Primeiros Exercícios/Program.cs
@@ -0,0 +1,29 @@
+﻿//entrada e saida de dados
+
+using System;
+class Program{
+
+static void Main(){
+
+    Console.WriteLine("Digite o seu nome: ");
+    string nome = Console.ReadLine ();
+
+    Console.WriteLine("Digite a sua idade: ");
+    int idade = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine($"Olá {nome}, seja bem vindo");
+        Console.WriteLine($"Você tem {idade} anos");
+
+
+        
+
+        
+
+
+
+
+}
+
+
+}
